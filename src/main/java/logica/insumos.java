73c83dd108bde5fd89f49bc792520203c9b76ea9
Adriana Feijoo <Adriana Feijoo@LAPTOP-3SO47TR6>
2021-10-05 19:05:29 -0500

    Clases insumos y presupuesto

--- src/main/java/logica/insumos.java
+++ src/main/java/logica/insumos.java
@@ -5,10 +5,175 @@
  */
 package logica;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import persistencia.ConexionBD;
+
 /**
  *
  * @author Adriana Feijoo Almonacid
  */
 public class insumos {
+    private int id_insumo;
+    private String nombre_material;
+    private int unidad;
+    private String rendimiento;
+    private double precio;
     
+    public insumos(){
+
+}
+    
+    public insumos(String nombre_material, int unidad, String rendimiento, double precio){
+        this.nombre_material = nombre_material;
+        this.unidad = unidad;
+        this.rendimiento = rendimiento;
+        this.precio = precio;
+    }
+
+    public int getId_insumo() {
+        return id_insumo;
+    }
+
+    public void setId_insumo(int id_insumo) {
+        this.id_insumo = id_insumo;
+    }
+
+    public String getNombre_material() {
+        return nombre_material;
+    }
+
+    public void setNombre_material(String nombre_material) {
+        this.nombre_material = nombre_material;
+    }
+
+    public int getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(int unidad) {
+        this.unidad = unidad;
+    }
+
+    public String getRendimiento() {
+        return rendimiento;
+    }
+
+    public void setRendimiento(String rendimiento) {
+        this.rendimiento = rendimiento;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    public boolean guardarinsumos(){
+        ConexionBD conexion = new ConexionBD();
+        String sentencia ="INSERT INTO insumos(id_insumo,nombre_material,unidad,rendimiento,precio)"
+                +"VALUES ('"+this.id_insumo+"','"+this.nombre_material+"','"+this.unidad+"','"+this.rendimiento+"','"+this.precio+"');";
+        if(conexion.setAutoCommitBD(false)){
+            if(conexion.insertarBD(sentencia)){
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            }else{
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        }else{
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+    
+    public boolean borrarinsumos(int id_insumo){
+        String Sentencia ="DELETE FROM 'insumos' WHERE 'id_insumo'='"+ id_insumo+"'";
+        ConexionBD conexion = new ConexionBD();
+        if(conexion.setAutoCommitBD(false)){
+            if(conexion.actualizarBD(Sentencia)){
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            }else{
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        }else{
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+    
+    public boolean actualizarinsumos() {
+        ConexionBD conexion = new ConexionBD();
+        String Sentencia = "UPDATE `insumos` SET id_insumo='" + this.id_insumo + "',nombre_material='" + this.nombre_material + "',unidad='" + this.unidad
+                + "',rendimiento='" + this.rendimiento + "',precio='" + this.precio + "' WHERE id_insumo=" + this.id_insumo + ";";
+        if (conexion.setAutoCommitBD(false)) {
+            if (conexion.actualizarBD(Sentencia)) {
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            } else {
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        } else {
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+
+    public List<insumos> listarinsumos() throws SQLException {
+        ConexionBD conexion = new ConexionBD();
+        List<insumos> listainsumos = new ArrayList<>();
+        String sql = "select * from insumos order by id_insumo asc";
+        ResultSet rs = conexion.consultarBD(sql);
+        insumos c;
+        while (rs.next()) {
+            c = new insumos();
+            c.setId_insumo(rs.getInt("id_insumo"));
+            c.setNombre_material(rs.getString("nombre_material"));
+            c.setUnidad(rs.getInt("unidad"));
+            c.setRendimiento(rs.getString("rendimiento"));
+            c.setPrecio(rs.getDouble("precio"));
+            listainsumos.add(c);
+
+        }
+        conexion.cerrarConexion();
+        return listainsumos;
+    }
+
+    public insumos getinsumos() throws SQLException {
+        ConexionBD conexion = new ConexionBD();
+        String sql = "select * from insumos where id_insumo='" + this.id_insumo + "'";
+        ResultSet rs = conexion.consultarBD(sql);
+        if (rs.next()) {
+            this.id_insumo = rs.getInt("id_insumo");
+            this.nombre_material = rs.getString("nombre_material");
+            this.unidad = rs.getInt("unidad");
+            this.rendimiento = rs.getString("rendimiento");
+            this.precio = rs.getDouble("precio");
+            conexion.cerrarConexion();
+            return this;
+
+        } else {
+            conexion.cerrarConexion();
+            return null;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "insumos{" + "id_insumo=" + id_insumo + ", nombre_material=" + nombre_material + ", unidad=" + unidad + ", rendimento=" + rendimiento + ", precio=" + precio + '}';
+    }
 }
--- src/main/java/logica/presupuestos.java
+++ src/main/java/logica/presupuestos.java
@@ -5,10 +5,175 @@
  */
 package logica;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import persistencia.ConexionBD;
+
 /**
  *
  * @author Adriana Feijoo Almonacid
  */
 public class presupuestos {
-    
+    private int id;
+     private String espacio;
+     private int medidas;
+     private String lista_materiales;
+     private double valor;
+     
+     public presupuestos(){
+         
+     }
+     
+     public presupuestos(String espacio, int medidas, String lista_materiales, double valor){
+         this.espacio = espacio;
+         this.medidas = medidas;
+         this.lista_materiales = lista_materiales;
+         this.valor = valor;
+     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEspacio() {
+        return espacio;
+    }
+
+    public void setEspacio(String espacio) {
+        this.espacio = espacio;
+    }
+
+    public int getMedidas() {
+        return medidas;
+    }
+
+    public void setMedidas(int medidas) {
+        this.medidas = medidas;
+    }
+
+    public String getLista_materiales() {
+        return lista_materiales;
+    }
+
+    public void setLista_materiales(String lista_materiales) {
+        this.lista_materiales = lista_materiales;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+     
+     public boolean guardarpresupuestos(){
+        ConexionBD conexion = new ConexionBD();
+        String sentencia ="INSERT INTO presupuesto_usuario(id,espacio,medidas,lista_materiales,valor)"
+                +"VALUES ('"+this.id+"','"+this.espacio+"','"+this.medidas+"','"+this.lista_materiales+"','"+this.valor+"');";
+        if(conexion.setAutoCommitBD(false)){
+            if(conexion.insertarBD(sentencia)){
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            }else{
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        }else{
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+     
+     public boolean borrarpresupuesto(int id){
+        String Sentencia ="DELETE FROM 'presupuesto_usuario' WHERE 'id'='"+ id+"'";
+        ConexionBD conexion = new ConexionBD();
+        if(conexion.setAutoCommitBD(false)){
+            if(conexion.actualizarBD(Sentencia)){
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            }else{
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        }else{
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+     
+     public boolean actualizarpresupuestos() {
+        ConexionBD conexion = new ConexionBD();
+        String Sentencia = "UPDATE `presupuesto_usuario` SET id='" + this.id + "',espacio='" + this.espacio + "',medidas='" + this.medidas
+                + "',lista_materiales='" + this.lista_materiales + "',valor='" + this.valor + "' WHERE id=" + this.id + ";";
+        if (conexion.setAutoCommitBD(false)) {
+            if (conexion.actualizarBD(Sentencia)) {
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            } else {
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        } else {
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+     
+     public List<presupuestos> listarpresupuestos() throws SQLException {
+        ConexionBD conexion = new ConexionBD();
+        List<presupuestos> listapresupuestos = new ArrayList<>();
+        String sql = "select * from presupuesto_usuario order by id asc";
+        ResultSet rs = conexion.consultarBD(sql);
+        presupuestos c;
+        while (rs.next()) {
+            c = new presupuestos();
+            c.setId(rs.getInt("id"));
+            c.setEspacio(rs.getString("espacio"));
+            c.setMedidas(rs.getInt("medidas"));
+            c.setLista_materiales(rs.getString("lista_materiales"));
+            c.setValor(rs.getDouble("valor"));
+            listapresupuestos.add(c);
+
+        }
+        conexion.cerrarConexion();
+        return listapresupuestos;
+    }
+     
+      public presupuestos getpresupuestos() throws SQLException {
+        ConexionBD conexion = new ConexionBD();
+        String sql = "select * from presupuesto_usuario where id='" + this.id + "'";
+        ResultSet rs = conexion.consultarBD(sql);
+        if (rs.next()) {
+            this.id = rs.getInt("id");
+            this.espacio = rs.getString("espacio");
+            this.medidas = rs.getInt("medidas");
+            this.lista_materiales = rs.getString("lista_materiales");
+            this.valor = rs.getDouble("valor");
+            conexion.cerrarConexion();
+            return this;
+
+        } else {
+            conexion.cerrarConexion();
+            return null;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "presupuesto_usuario{" + "id=" + id + ", espacio=" + espacio + ", medidas=" + medidas + ", lista_materiales=" + lista_materiales + ", valor=" + valor + '}';
+    }
 }
